de6fd0b014988b83a364e9a3675964881a70ae06
Florian Trabauer <florian@trabauer.com>
2025-04-21 12:18:55 +0200

    removed old function I don't use anymore
    I've written a small python tool to configure my network.

--- zsh/my-functions.sh
+++ zsh/my-functions.sh
@@ -1,40 +1,3 @@
-#$PDF_VIEWER="evince"
-
-function look-for-invoice() {
-	# Searches for the latest invoice (specify a pattern ) and opens it with evince
-  pattern=$1
-  evince $(pdfgrep -i -R $pattern $HOME/Documents/PDF_Rechnungssablage/201[7-8]*.pdf | cut -d ':' -f 1 | tail -n 1)
-}
-
-function setup-arch() {
-  install-archlinux-packages
-  install-oh-my-zsh
-  join-groups
-  enable-services
-}
-
-function setup-arch-i3() {
-  sudo pacman -S i3 
-}
-
-function setup-debian() {
-  install-debian-packages
-  install-oh-my-zsh
-}
-
-
-function install-archlinux-packages() {
-  sudo pacman -S --needed $(cat $HOME/dotfiles/required-archlinux-packages.txt)
-}
-
-function enable-services() {
-  sudo systemctl enable pcscd.service
-}
-
-function join-groups() {
-  sudo usermod $USER -a -G kismet,wireshark
-}
-
 function install-oh-my-zsh() {
   # Use colors, but only if connected to a terminal, and that terminal
   # supports them.
@@ -102,74 +65,6 @@ function install-oh-my-zsh() {
   }
 }
 
-function setup-tmux() {
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-}
-
-
-function nmcli-road() {
-  # Setup network for remote work
-  # Celular network
-  for i in {1..5}; do
-  	nmcli con up 'Drei (3) Default'
-	if [ $? -eq 0 ]; then
-		break
-	fi
-	sleep 5
-  done
-
- # Hotspot for printer and phone
- for i in {1..5}; do 
-  	nmcli con up Hotspot
-  	if [ $? -eq 0 ]; then
-		break
-	fi
-	sleep 5
-  done
-
- # Wireguard Connnection to the Office
- # nmcli con up wg0
- for i in {1..5}; do
-  	sudo wg-quick up wg0 # More reliable than nmcli
-	if [ $? -eq 0 ]; then
-		break
-	fi
-	sleep 5
-  done
-}
-
-function nmcli-home() {
-  # Setup network for home-office
-  #nmcli con down wg0
-  sudo wg-quick down wg0 # More reliable than nmcli
-  echo $?
-  nmcli con down 'Drei (3) Default'
-  echo $?
-  nmcli con up trabauer50
-  echo $?
-}
-
-
-function start-work-apps() {
-	echo "starting work Apps"
-	chromium &
-	thunderbird &
-}
-
-
-function remote-work() {
-	# Wait a little 3G Modem needs some time to work after boot
-	sleep 5
-	nmcli-road
-	sleep 5
-	#sudo wg-quick up wg0
-	start-work-apps
-}
-
-function home-work() {
-	nmcli-home
-	start-work-apps
-}
 
 function mutt-office() {
   mutt -F "~/.mutt/muttrc-office"
@@ -179,30 +74,4 @@ function lblk() {
   lsblk -o NAME,SIZE,TYPE,FSTYPE,PTTYPE,PARTLABEL,LABEL,MODEL,MOUNTPOINT
 }
 
-function vpn-home() {
-  if [ "$1" = "up" ]; then
-    echo "vpn up"
-    sudo wg-qick up wg0
-    #nmcli con up wg0
-  elif [ "$1" = "down" ]; then;
-    echo "vpn down"
-    sudo wg-quick down wg0
-    # nmcli con down wg0
-  else
-    echo "usage: vpn-home up|down"
-  fi
-}
-
-function shutdown-carpi() {
-  ssh pi@192.168.199.254 shutdown now
-}
-
-function chia-start-all-if-needed() {
-  if which chia > /dev/null; then
-    if [ ! -f $HOME/.chia/mainnet/run/chia_full_node.pid ]; then
-      echo "Chia not running starting chia!"
-      chia start all
-    fi
-  fi
-}
 
