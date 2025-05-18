05be7164707ac934e5c052221f77eab5a6f48370
Florian Trabauer <florian@trabauer.com>
2025-05-18 15:36:46 +0200

    first custom version of wallpaper script
    It uses subfolders in ~/Pictures/Wallpapers/ to select a Wallpaper that
    fits the focused monitor.
    
    The script needs still improvement to launch at start of hyprland (cycle
    through monitors)

--- config/hypr/random-wallpaper.sh
+++ config/hypr/random-wallpaper.sh
@@ -1,22 +1,61 @@
-#!/usr/bin/env bash
-# 
-# WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
-# CURRENT_WALL=$(hyprctl hyprpaper listloaded)
-# # Get the name of the focused monitor with hyprctl
-# FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
-# # Get a random wallpaper that is not the current one
-# WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-# 
-# # Apply the selected wallpaper
-# hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
-# 
+#!/usr/bin/env zsh
+set -x
 
-WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
+
+# 
+WALLPAPER_W_DIR="$HOME/Pictures/Wallpapers/wide" # 16:9
+WALLPAPER_UW_DIR="$HOME/Pictures/Wallpapers/ultra-wide" # 21:9 
+WALLPAPER_DUAL_DIR="$HOME/Pictures/Wallpapers/dual" # 32:9
+WALLPAPER_VERTICAL_DIR="$HOME/Pictures/Wallpapers/vertical" # vertical
+
+
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+# Get the name of the focused monitor with hyprctl
+FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+FOCUSED_MONITOR_TRANSFORM=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .transform')
 
+if [[ $FOCUSED_MONITOR_TRANSFORM -eq 0 || $FOCUSED_MONITOR_TRANSFORM -eq 2 ]]; then #Dislpay normal or up side down 
+    echo "Monitor Horizontal"
+    typeset -F FM_WIDTH
+    typeset -F FM_HEIGHT
+    FM_WIDTH=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .width')
+    FM_HEIGHT=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .height')
+    if [[ $FM_WIDTH/$FM_HEIGHT -lt 1.3 ]]; then
+        echo "Monitor 5:4"
+        WALLPAPER_DIR=$WALLPAPER_W_DIR
+    elif [[ $FM_WIDTH/$FM_HEIGHT -lt 1.65 ]]; then
+        echo "Monitor 16:10"
+        WALLPAPER_DIR=$WALLPAPER_W_DIR
+    elif [[ $FM_WIDTH/$FM_HEIGHT -lt 1.8 ]]; then
+        echo "Monitor 16:9"
+        WALLPAPER_DIR=$WALLPAPER_W_DIR
+    elif [[ $FM_WIDTH/$FM_HEIGHT -lt 2.39 ]]; then
+        echo "Monitor 21:9"
+        WALLPAPER_DIR=$WALLPAPER_UW_DIR
+    elif [[ $FM_WIDTH/$FM_HEIGHT -lt 2.45 ]]; then
+        echo "Monitor 24:10"
+        WALLPAPER_DIR=$WALLPAPER_UW_DIR
+    else #  [[ $FM_WIDTH/$FM_HEIGHT -gt 3.5 ]]; then
+        echo "Monitor 32:9"
+        WALLPAPER_DIR=$WALLPAPER_DUAL_DIR
+    fi
+elif [[  $FOCUSED_MONITOR_TRANSFORM -eq 1 || $FOCUSED_MONITOR_TRANSFORM -eq 3 ]]; then
+    echo "Monitor Vertical"
+    WALLPAPER_DIR=$WALLPAPER_VERTICAL_DIR
+fi
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
 
+
+# WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
+# CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+# 
+# # Get a random wallpaper that is not the current one
+# WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# 
+# # Apply the selected wallpaper
+# hyprctl hyprpaper reload ,"$WALLPAPER"
+# 
